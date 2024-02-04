94bbf0a38f53460127b3bcb1f4cdf22f22e5befe
Michael Golshani <michaelgolshani@gmail.com>
2024-02-03 23:52:22 -0800

    before login is defined but never used typescript error

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -45,7 +45,7 @@ export default buildConfig({
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      // beforeLogin: [BeforeLogin],
+      beforeLogin: [BeforeLogin],
       // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
       beforeDashboard: [BeforeDashboard],
